4d5731b50897a34f2973d71bf1a48b71e894656c
Julien Baley <julien.baley@gmail.com>
2014-03-12 16:53:59 +0000

    Makes use of mod's parameter to allow user to customize their templates on the fly

--- abjad_diacritics.py
+++ abjad_diacritics.py
@@ -2,7 +2,27 @@
 # Copyright: Julien Baley <julien.baley@gmail.com>
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-#TO CUSTOMIZE THIS PLUGIN, SEE COMMENT AT THE BOTTOM OF THE FILE!
+#USER MANUAL:
+#This plug-in helps user removing diacritics from Arabic, Hebrew and Syriac.
+#This can for instance be used to display the sentence without diacritics
+#in the question, and display them in the answer. 
+
+#For Arabic, if your current template is {{Front}}, replace it 
+#with {{plain_arabic_except(hamza, shadda):Front}} #to display Arabic
+#stripped of all diacritics except hamza and shadda. For completely 
+#plain Arabic, have either {{plain_arabic_except:Field}}
+#or {{plain_arabic_except():Field}}.
+
+#Supported exceptions can be found in the dictionary just below. If the diacritic
+#you want to retain is not there, find its Unicode point, add it in the relevant
+#section and restart Anki.
+
+characters = {'arabic':{'hamza': [0x0621,0x654,0x655],
+                        'shadda':[0x0651,]},
+              'hebrew':{},
+              'syriac':{}}
+
+##THE CODE THEREAFTER SHOULDN'T BE MODIFIED UNLESS YOU KNOW WHAT YOU DO#
 
 from anki.hooks import addHook
 from itertools import chain
@@ -22,20 +42,23 @@ unicode_ranges = {'arabic':set(chain(range(0x0600, 0x06FF),
 class Hook():
   def __init__(self, name, script, exceptions=[]):
     assert script in supported_scripts
+    self.script = script
     self.incl = unicode_ranges[script]
-    self.excl = [unichr(c) for c in exceptions]
     addHook('fmod_'+name, self.remove_diacritics)
   
-  def remove_diacritics(self, txt, extra, *args): #extra/args are passed by Anki, not used
+  def remove_diacritics(self, txt, exc, *_):
     nkfd_form = unicodedata.normalize('NFKD', txt)
+    try:
+      excl = [unichr(c) for k in exc.split(',') for c in characters[self.script][k]]
+    except (AttributeError, KeyError):
+      excl = set()
     keep = "".join([c for c in nkfd_form if \
                     not (ord(c) in self.incl and unicodedata.combining(c)) \
-                    or c in self.excl])
+                    or c in excl])
     return unicodedata.normalize('NFKC',keep)
 
 
 #ADD YOUR HOOKS HERE
-Hook(name='plain_arabic',script='arabic')
-Hook(name='keep_shadda', script='arabic',exceptions=[0x0651,])
-Hook(name='plain_hebrew',script='hebrew')
-Hook(name='plain_syriac',script='syriac')
+Hook(name='plain_arabic_except',script='arabic')
+Hook(name='plain_hebrew_except',script='hebrew')
+Hook(name='plain_syriac_except',script='syriac')
