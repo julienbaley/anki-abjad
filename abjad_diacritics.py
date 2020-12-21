4c17d7e8237866122d5c562b619b450347312357
Julien Baley <julien.baley@gmail.com>
2020-12-21 13:49:49 +0000

    Add support for Anki 2.1 addons

--- abjad_diacritics.py
+++ abjad_diacritics.py
@@ -10,27 +10,26 @@
 # For Arabic, if your current template is {{Front}}, replace it
 # with {{plain_arabic_except(hamza, shadda):Front}} #to display Arabic
 # stripped of all diacritics except hamza and shadda. For completely
-# plain Arabic, have either {{plain_arabic_except:Field}}
-# or {{plain_arabic_except():Field}}.
+# plain Arabic, use {{plain_arabic_except():Field}}.
 
 # Supported exceptions can be found in the dictionary just below. If the
 # diacritic you want to retain is not there, find its Unicode point, add it in
 # the relevant section and restart Anki.
 
-characters = {'arabic': {'hamza': [0x0621, 0x654, 0x655],
+CHARACTERS = {'arabic': {'hamza': [0x0621, 0x654, 0x655],
                          'shadda': [0x0651]},
               'hebrew': {},
               'syriac': {}}
 
 # THE CODE THEREAFTER SHOULDN'T BE MODIFIED UNLESS YOU KNOW WHAT YOU DO #
 
-from anki.hooks import addHook
-from itertools import chain
+import re
 import unicodedata
+from itertools import chain
 
-supported_scripts = ['arabic', 'hebrew', 'syriac']
+import anki
 
-unicode_ranges = {'arabic': set(chain(range(0x0600, 0x06FF),
+UNICODE_RANGES = {'arabic': set(chain(range(0x0600, 0x06FF),
                                       range(0x0750, 0x077F),
                                       range(0x08A0, 0x08FF),
                                       range(0xFB50, 0xFDFF),
@@ -38,30 +37,33 @@ unicode_ranges = {'arabic': set(chain(range(0x0600, 0x06FF),
                                       range(0x10E60, 0x10E7F))),
                   'hebrew': set(range(0x0590, 0x05FF)),
                   'syriac': set(range(0x0700, 0x074F))}
+SUPPORTED_SCRIPTS = set(UNICODE_RANGES)
+PATTERN = r'plain_(?P<lang>{langs})_except\((?P<excs>.*?)\)'
+
+FILTER_MATCHER = re.compile(PATTERN.format(langs='|'.join(SUPPORTED_SCRIPTS)))
 
 
-class Hook():
-    def __init__(self, name, script, exceptions=[]):
-        assert script in supported_scripts
-        self.script = script
-        self.incl = unicode_ranges[script]
-        addHook('fmod_'+name, self.remove_diacritics)
-
-    def remove_diacritics(self, txt, exc, *_):
-        nkfd_form = unicodedata.normalize('NFKD', unicode(txt))
+def remove_diacritics_filter(field_text, field_name, filter_name, _):
+    match = FILTER_MATCHER.match(filter_name)
+    if match:
+        lang = match.group('lang')
+        incl = UNICODE_RANGES[lang]
         try:
-            excl = [unichr(c)
-                    for k in exc.split(',')
-                    for c in characters[self.script][k]]
-        except (AttributeError, KeyError):
+            excl = {chr(c)
+                    for k in match.group('excs').split(',')
+                    for c in CHARACTERS[lang][k]}
+        except (AttributeError, KeyError) as e:
             excl = set()
-        keep = "".join([c for c in nkfd_form if
-                        not (ord(c) in self.incl and unicodedata.combining(c))
-                        or c in excl])
-        return unicodedata.normalize('NFKC', unicode(keep))
 
+        nkfd_form = unicodedata.normalize('NFKD', field_text)
+        keep = ''.join(c
+                       for c in nkfd_form
+                       if ord(c) not in incl  # not a char from desired script
+                       or c in excl  # explicitly excluded from removal
+                       or not unicodedata.combining(c)  # not a diacritic
+                       )
+        field_text = unicodedata.normalize('NFKC', keep)
 
-# ADD YOUR HOOKS HERE
-Hook(name='plain_arabic_except', script='arabic')
-Hook(name='plain_hebrew_except', script='hebrew')
-Hook(name='plain_syriac_except', script='syriac')
+    return field_text
+
+anki.hooks.field_filter.append(remove_diacritics_filter)
